c6ab092e7f276fe8d11c7a5d021670207ccb631a
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2021-04-19 23:46:35 -0500

    Feat: UserFactory test

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,19 +2,21 @@
 
 namespace Database\Factories;
 
-use App\Models\USer;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 use Bouncer;
 
 class UserFactory extends Factory
 {
+
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = USer::class;
+    protected $model = User::class;
 
     /**
      * Define the model's default state.
@@ -30,16 +32,23 @@ class UserFactory extends Factory
             'username' => $this->faker->unique()->userName,
             'password' => 'MiPassword@',
             'phone' => $this->faker->numerify('9########'),
-            // 'birthday' => $this->faker->date(),
+            'birthday' => $this->faker->dateTimeBetween('-50 years', '-18 years'),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
     }
 
-    public function configure()
+    public function withAdminRole()
     {
         return $this->afterCreating(function (User $user) {
-            Bouncer::assign('user')->to($user);
+            Bouncer::assign(Role::ADMIN)->to($user);
+        });
+    }
+
+    public function withUserRole()
+    {
+        return $this->afterCreating(function (User $user) {
+            Bouncer::assign(Role::USER)->to($user);
         });
     }
 }
---
+++ tests/Feature/UserFactoryTest.php
@@ -0,0 +1,52 @@
+<?php
+
+
+use App\Models\Role;
+use App\Models\User;
+use Database\Factories\UserFactory;
+use Database\Seeders\RoleSeeder;
+use Tests\TestCase;
+
+class UserFactoryTest extends TestCase
+{
+    /** @test */
+    public function create_a_system_user_with_user_role()
+    {
+        $this->seed(RoleSeeder::class);
+        $role = Role::whereName(Role::USER)->first();
+
+        $user = User::factory()->withUserRole()->create();
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'email' => $user->email,
+        ]);
+        $this->assertDatabaseHas('assigned_roles', [
+            'role_id' => $role->id,
+            'entity_id' => $user->id,
+            'entity_type' => User::class
+        ]);
+    }
+    /** @test */
+    public function create_a_system_user_with_admin_role()
+    {
+        $this->seed(RoleSeeder::class);
+        $role = Role::whereName(Role::ADMIN)->first();
+
+        $user = User::factory()->withAdminRole()->create();
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'email' => $user->email,
+        ]);
+        $this->assertDatabaseHas('assigned_roles', [
+            'role_id' => $role->id,
+            'entity_id' => $user->id,
+            'entity_type' => User::class
+        ]);
+    }
+}
