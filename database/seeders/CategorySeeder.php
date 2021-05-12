4eb63c1f63472c07727e3a63f64de9de72c399fe
Kenny Horna <kenny.horna@aboutyou.com>
2021-05-12 16:58:44 -0500

    Seeder updated

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,42 +16,68 @@ class CategorySeeder extends Seeder
     {
         Category::factory()->create([
             'name' => 'Bed',
+            'parent_id' => null,
+            'active' => true,
         ]);
         Category::factory()->create([
             'name' => 'Chair',
+            'parent_id' => null,
+            'active' => true,
         ]);
         Category::factory()->create([
             'name' => 'Coffee Table',
+            'parent_id' => null,
+            'active' => true,
         ]);
         Category::factory()->create([
             'name' => 'Console',
+            'parent_id' => null,
+            'active' => true,
         ]);
         Category::factory()->create([
             'name' => 'Desk',
+            'parent_id' => null,
+            'active' => true,
         ]);
         Category::factory()->create([
             'name' => 'Dining Chair',
+            'parent_id' => null,
+            'active' => true,
         ]);
         Category::factory()->create([
             'name' => 'Dining Table',
+            'parent_id' => null,
+            'active' => true,
         ]);
         Category::factory()->create([
             'name' => 'Dresser',
+            'parent_id' => null,
+            'active' => true,
         ]);
         Category::factory()->create([
             'name' => 'Nightstand',
+            'parent_id' => null,
+            'active' => true,
         ]);
         Category::factory()->create([
             'name' => 'Ottoman',
+            'parent_id' => null,
+            'active' => true,
         ]);
         Category::factory()->create([
             'name' => 'Shelf',
+            'parent_id' => null,
+            'active' => true,
         ]);
         Category::factory()->create([
             'name' => 'Side Table',
+            'parent_id' => null,
+            'active' => true,
         ]);
         Category::factory()->create([
             'name' => 'Sofa',
+            'parent_id' => null,
+            'active' => true,
         ]);
     }
 }
