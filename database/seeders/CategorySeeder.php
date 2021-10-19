037e1615e6377290cb90af86a89734bbfb800b99
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2021-10-19 15:37:43 -0500

    Updated CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,75 +14,26 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        Category::factory()->create([
-            'name' => 'Bed',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Chair',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Coffee Table',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Console',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Desk',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Dining Chair',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Dining Table',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Dresser',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Nightstand',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Ottoman',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Shelf',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Side Table',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Sofa',
-            'parent_id' => null,
-            'active' => true,
-        ]);
-        Category::factory()->create([
-            'name' => 'Tiles',
-            'parent_id' => null,
-            'active' => true,
-        ]);
+        collect([
+            ['name' => 'Bed'],
+            ['name' => 'Chair'],
+            ['name' => 'Coffee Table'],
+            ['name' => 'Console'],
+            ['name' => 'Desk'],
+            ['name' => 'Dining Chair'],
+            ['name' => 'Dining Table'],
+            ['name' => 'Dresser'],
+            ['name' => 'Nightstand'],
+            ['name' => 'Ottoman'],
+            ['name' => 'Shelf'],
+            ['name' => 'Side Table'],
+            ['name' => 'Sofa'],
+        ])->each(function ($category){
+            Category::factory()->create([
+                'name' => $category['name'],
+                'parent_id' => null,
+                'active' => true,
+            ]);
+        });
     }
 }
