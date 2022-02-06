f5222ff9c07aa6ec6f5eaef03a387cfc12ae5725
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2022-02-05 22:28:10 -0500

    Removed unused class in models

--- app/Models/UnitSystem.php
+++ app/Models/UnitSystem.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Database\Factories\UnitSystemFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
