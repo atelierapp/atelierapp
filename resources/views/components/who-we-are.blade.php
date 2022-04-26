7a073c430f56eb9736c6438680707313afeb2786
Kenny Horna <kenny.horna@aboutyou.com>
2022-04-26 23:53:44 +0200

    Animations added

--- resources/views/components/who-we-are.blade.php
+++ resources/views/components/who-we-are.blade.php
@@ -3,35 +3,20 @@
     <div class="bg-white h-1/3 sm:h-2/3"></div>
   </div>
   <div class="relative max-w-7xl flex items-center flex-col" x-data>
-    <div class="text-center flex flex-col space-y-4 w-4/5">
-      <h2
-          class="md:text-center text-2xl font-medium text-gray-900 tracking-wide sm:text-4xl mt-4"
-          x-intersect="$el.classList.add('slide-in-bottom')"
-      >
+    <div class="text-center flex flex-col space-y-4 w-4/5" x-intersect="$el.classList.add('slide-in-bottom')">
+      <h2 class="md:text-center text-2xl font-medium text-gray-900 tracking-wide sm:text-4xl mt-4">
         Who we are
       </h2>
-      <div
-          class="mx-auto py-6"
-          x-intersect="$el.classList.add('slide-in-bottom')"
-      >
+      <div class="mx-auto py-6">
         <div class="h-1 bg-gray-500 w-20"></div>
       </div>
-      <p
-          class="text-md tracking-wide leading-8"
-        x-intersect="$el.classList.add('slide-in-bottom')"
-      >
+      <p class="text-md tracking-wide leading-8">
         We are a space for people who love design and are also conscious of the impact that they leave behind.
       </p>
-      <p
-          class="text-md tracking-wide leading-8"
-        x-intersect="$el.classList.add('slide-in-bottom')"
-      >
+      <p class="text-md tracking-wide leading-8">
         Atelier Home Design is the platform that makes interior design an easy and fun process. It provides users with a 3D design platform that makes everything — from envisioning the project to executing it in real life — a simple, fun, and useful process.
       </p>
-      <p
-          class="text-md tracking-wide leading-8"
-        x-intersect="$el.classList.add('slide-in-bottom')"
-      >
+      <p class="text-md tracking-wide leading-8">
         We make sure to provide only good quality products by providing a channel for independent designers and makers to reach you through their Atelier shops. These small brands do not mass-produce, they source their products ethically, and they’ll make sure your design is always one of a kind!
       </p>
     </div>
