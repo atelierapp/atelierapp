40b4874915d844f027eb81e182f223fd2576cbf3
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2022-04-27 07:53:45 -0500

    updated ProducService to create product

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -2,13 +2,38 @@
 
 namespace App\Services;
 
+use App\Models\Product;
+
 class ProductService
 {
     private string $path = 'stores';
 
-    public function __construct(private MediaService $mediaService)
+    public function __construct()
     {
         //
+    }
+
+    public function store(array $params): Product
+    {
+        $data = $params;
+        $product = Product::create($data);
+        $this->processCategories($product, [$data['category_id']]);
+
+        return $product;
+    }
+
+    public function processCategories(int|Product $product, array $categories): void
+    {
+        if (is_int($product)) {
+            $product = $this->getBy($product);
+        }
+
+        $product->categories()->sync($categories);
+    }
+
+    public function getBy(int $product, string $field = 'id')
+    {
+        return Product::where($field, '=', $product)->firstOrFail();
     }
 
 }
