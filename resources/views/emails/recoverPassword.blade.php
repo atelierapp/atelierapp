2781c69fe50db10c8b78365fc5bd70cba2c6509d
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2022-06-05 10:36:11 -0500

    updated view to send link to recover password

---
+++ resources/views/emails/recoverPassword.blade.php
@@ -0,0 +1,3 @@
+<head></head>
+<body><p> You recently requested a password reset with us.</p>
+<p>Please click <a href='$ConfirmationUrl$'>here</a> to be directed to a page to reset your password. Thanks!</p>
