4095666e9e258b19e23a5ef881f9524286aeccc8
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2022-06-07 01:32:30 -0500

    updated UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,23 +5,24 @@ namespace App\Http\Resources;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /** @mixin \App\Models\User */
-class UserResource extends JsonResource {
+class UserResource extends JsonResource
+{
     /**
      * @param \Illuminate\Http\Request $request
      * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
         return [
-            'id'         => $this->id,
+            'id' => $this->id,
             'first_name' => $this->first_name,
-            'last_name'  => $this->last_name,
-            'email'      => $this->email,
-            'username'   => $this->username,
-            'phone'      => $this->phone,
-            'birthday'   => $this->birthdat ? $this->birthday->format('m/d/Y') : null,
-            'avatar'     => $this->avatar,
-            'is_active'  => $this->is_active,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'username' => $this->username,
+            'phone' => $this->phone,
+            'birthday' => $this->birthdat ? $this->birthday->format('m/d/Y') : null,
+            'avatar' => $this->avatar,
+            'is_active' => $this->is_active,
             'created_at' => $this->created_at->format('m/d/Y H:i:s'),
             'updated_at' => $this->updated_at->format('m/d/Y H:i:s'),
         ];
