312487c88bec68f300a983f5ed315cc2d9958d1a
Kenny Horna <kenny.horna@aboutyou.com>
2022-06-17 23:52:22 +0200

    Template logo replaced

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('storage/atelier-logo.png') }}" class="logo" alt="Atelier Home Design">
+<img src="https://atelier-production-bucket.s3.amazonaws.com/atelier-logo.png" class="logo" alt="Atelier Home Design">
 </a>
 </td>
 </tr>
