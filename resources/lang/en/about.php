849634696e5fd23c8bc56025dfccf5561cd4672f
Kenny Horna <kennyhorna@gmail.com>
2022-10-07 18:22:18 +0200

    About translations fixed

--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'about-us' => 'Sobre Nosotros',
-    'what-we-do' => 'Qué hacemos',
-    'who-we-are' => 'Quiénes somos',
-    'we-are-a-group-that-loves-design' => 'Somos un espacio para las personas que aman el diseño y también son conscientes del impacto que dejan atrás.',
-    'we-make-designing-fun' => 'Atelier App es la plataforma que hace del diseño de interiores un proceso fácil y divertido. Proporciona a los usuarios una plataforma de diseño 3D que hace que todo, desde visualizar el proyecto hasta ejecutarlo en la vida real, sea un proceso simple, divertido y útil.',
-    'we-provide-good-quality-products' => 'Nos aseguramos de ofrecer solo productos de buena calidad proporcionando un canal para que los diseñadores y fabricantes independientes se comuniquen contigo a través de sus tiendas Atelier. ¡Estas pequeñas marcas no producen en masa, obtienen sus productos de manera ética y se asegurarán de que su diseño sea siempre único!',
-    'some-of-the-brands-that-work-with-us' => 'Algunas marcas con las que trabajamos',
+    'about-us' => 'About us',
+    'what-we-do' => 'What we do',
+    'who-we-are' => 'Who we are',
+    'we-are-a-group-that-loves-design' => 'We are a space for people who love design and are also conscious of the impact that they leave behind.',
+    'we-make-designing-fun' => 'Atelier App is the platform that makes interior design an easy and fun process. It provides users with a 3D design platform that makes everything — from envisioning the project to executing it in real life — a simple, fun, and useful process.',
+    'we-provide-good-quality-products' => 'We make sure to provide only good quality products by providing a channel for independent designers and makers to reach you through their Atelier shops. These small brands do not mass-produce, they source their products ethically, and they’ll make sure your design is always one of a kind!',
+    'some-of-the-brands-that-work-with-us' => 'Some of the brands we work with',
 ];
