0f904f6713e2d244893ba4a3672dbed65198d81f
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2022-10-09 00:24:56 -0500

    Added model as base to manage data based in country by config

---
+++ app/Models/BaseModelCountry.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Scopes\CountryScope;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @mixin IdeHelperAbility
+ */
+class BaseModelCountry extends Model
+{
+    protected static function booted()
+    {
+        static::addGlobalScope(new CountryScope());
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->country)) {
+                $model->country = config('app.country');
+            }
+        });
+    }
+}
