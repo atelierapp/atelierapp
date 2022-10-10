f4f17ca857b718a97a9f76abfa55c1a90744b97f
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2022-10-10 08:16:44 -0500

    Added country state by config to CountryStateTrait

--- app/Traits/Factories/CountryStateTrait.php
+++ app/Traits/Factories/CountryStateTrait.php
@@ -4,6 +4,15 @@ namespace App\Traits\Factories;
 
 trait CountryStateTrait
 {
+    public function country()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'country' => config('app.country'),
+            ];
+        });
+    }
+
     public function us()
     {
         return $this->state(function (array $attributes) {
