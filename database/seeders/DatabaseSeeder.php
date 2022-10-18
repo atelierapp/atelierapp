3ab8c3473cb178facad3a904a6f3fcbe83a5ebe4
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2022-10-17 22:46:32 -0500

    Updated DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,27 +9,16 @@ class DatabaseSeeder extends Seeder
 
     public function run()
     {
-        $this->call(MediaTypeSeeder::class);
-        $this->call(ColorSeeder::class);
-        $this->call(StyleSeeder::class);
-        $this->call(CategorySeeder::class);
-        $this->call(QualitySeeder::class);
+        $this->call([
+            MediaTypeSeeder::class,
+            ColorSeeder::class,
+            StyleSeeder::class,
+            CategorySeeder::class,
+            QualitySeeder::class,
 
-        $this->call(BouncerSeeder::class);
-        $this->call(RoleSeeder::class);
-        $this->call(UserSeeder::class);
-
-        $this->call(DatabaseUsSeeder::class);
-
-        // if (app()->environment(['local', 'staging'])) {
-        //     $this->call(TagSeeder::class);
-        //     $this->call(MaterialSeeder::class);
-        //     $this->call(UnitSystemSeeder::class);
-        //     $this->call(UnitSeeder::class);
-        //     $this->call(ProjectSeeder::class);
-        //     $this->call(MediaSeeder::class);
-        //     $this->call(RoomSeeder::class);
-        // }
+            BouncerSeeder::class,
+            RoleSeeder::class,
+        ]);
     }
 
 }
