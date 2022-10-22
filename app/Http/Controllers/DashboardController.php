67a3ea4e0d3e963ba4ebf92df99adfd8bcf1623b
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2022-10-22 14:58:09 -0500

    Updating dashboard appis

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -64,52 +64,52 @@ class DashboardController extends Controller
     {
         $values = [];
 
-        for ($i = 0; $i < 6; $i++) {
-            $tmp = [
-                'month' => Str::title(now()->subMonths($i)->monthName),
-                'sales' => rand(10000, 30000),
-                'views' => rand(10000, 30000),
-            ];
-
-            $values[] = $tmp;
-        }
+        // for ($i = 0; $i < 6; $i++) {
+        //     $tmp = [
+        //         'month' => Str::title(now()->subMonths($i)->monthName),
+        //         'sales' => rand(10000, 30000),
+        //         'views' => rand(10000, 30000),
+        //     ];
+        //
+        //     $values[] = $tmp;
+        // }
 
         return response()->json(['data' => $values]);
     }
 
     public function orders()
     {
-        $products = Product::query()->inRandomOrder()->take(10)->get();
+        // $products = Product::query()->inRandomOrder()->take(10)->get();
         $data = [];
 
-        foreach ($products as $product) {
-            $tmp = [
-                'product' => $product->title,
-                'price' => $product->price,
-                'is_accepted' => (boolean) rand(0, 1),
-                'email' => User::query()->inRandomOrder()->first()->email,
-                'delivery_notes' => 'lorem ipsum',
-            ];
-            $data[] = $tmp;
-        }
+        // foreach ($products as $product) {
+        //     $tmp = [
+        //         'product' => $product->title,
+        //         'price' => $product->price,
+        //         'is_accepted' => (boolean) rand(0, 1),
+        //         'email' => User::query()->inRandomOrder()->first()->email,
+        //         'delivery_notes' => 'lorem ipsum',
+        //     ];
+        //     $data[] = $tmp;
+        // }
 
         return response()->json(['data' => $data]);
     }
 
     public function topProduct()
     {
-        $products = Product::query()->inRandomOrder()->take(5)->get();
+        // $products = Product::query()->inRandomOrder()->take(5)->get();
         $data = [];
 
-        foreach ($products as $product) {
-            $tmp = [
-                'image' => $product->featured_media->url,
-                'name' => $product->title,
-                'availability' => rand(0, 150),
-                'sales' => rand(10000, 30000),
-            ];
-            $data[] = $tmp;
-        }
+        // foreach ($products as $product) {
+        //     $tmp = [
+        //         'image' => $product->featured_media->url,
+        //         'name' => $product->title,
+        //         'availability' => rand(0, 150),
+        //         'sales' => rand(10000, 30000),
+        //     ];
+        //     $data[] = $tmp;
+        // }
 
         return response()->json(['data' => $data]);
     }
@@ -117,11 +117,16 @@ class DashboardController extends Controller
     public function quickDetails()
     {
         return response()->json(['data' => [
-            'customers' => rand(50, 150),
-            'awaiting_orders' => rand(150, 300),
-            'on_hold_orders' => rand(50, 150),
-            'low_stock_orders' => rand(25, 75),
-            'out_stock_orders' => rand(0, 25),
+            // 'customers' => rand(50, 150),
+            // 'awaiting_orders' => rand(150, 300),
+            // 'on_hold_orders' => rand(50, 150),
+            // 'low_stock_orders' => rand(25, 75),
+            // 'out_stock_orders' => rand(0, 25),
+            'customers' => 0,
+            'awaiting_orders' => 0,
+            'on_hold_orders' => 0,
+            'low_stock_orders' => 0,
+            'out_stock_orders' => 0,
         ]]);
     }
 
