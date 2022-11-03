46abc3d50855d34051afe4c25a299097de82f1ba
Kenny Horna <kennyhorna@gmail.com>
2022-11-03 23:09:07 +0100

    Temp fix

--- app/Http/Resources/ShoppingCartResource.php
+++ app/Http/Resources/ShoppingCartResource.php
@@ -13,6 +13,7 @@ class ShoppingCartResource extends JsonResource
     public function toArray($request): array
     {
         return [
+            'user_id' => $this->resource->customer_id, // temporary field. Should be removed after next iOS update
             'customer_id' => $this->resource->customer_id,
             'has_user' => $this->resource->customer_type === User::class,
             'store_name' => $this->variation->product->store->name,
