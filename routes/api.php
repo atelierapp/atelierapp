959e75e2ee58805f16c4c5c981cb6bf35da0b2d0
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2022-11-04 21:34:05 -0500

    renamed OrderDetailController to OrderProductController

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\MaterialController;
 use App\Http\Controllers\MediaController;
 use App\Http\Controllers\MediaTypeController;
 use App\Http\Controllers\OrderController;
-use App\Http\Controllers\OrderDetailController;
+use App\Http\Controllers\OrderProductController;
 use App\Http\Controllers\PaypalController;
 use App\Http\Controllers\PlanController;
 use App\Http\Controllers\Process\ProductProjectController;
@@ -159,9 +159,9 @@ Route::middleware('locale')->group(function () {
         // Orders
         Route::post('shopping-cart/create-order', [OrderController::class, 'store'])->name('shopping-cart.order');
         Route::apiResource('orders', OrderController::class)->names('order')->only(['index', 'update', 'show']);
-        Route::get('orders/{order}/details', [OrderDetailController::class, 'index'])->name('order.details');
+        Route::get('orders/{order}/details', [OrderProductController::class, 'index'])->name('order.details');
         Route::post('orders/{order}/accept', [OrderController::class, 'accept'])->name('order.accept');
-        Route::patch('orders/{order}/details/{detail}', [OrderDetailController::class, 'update'])->name('order.details.update');
+        Route::patch('orders/{order}/details/{detail}', [OrderProductController::class, 'update'])->name('order.details.update');
 
         // Dashboard
         Route::prefix('dashboard')->group(function () {
