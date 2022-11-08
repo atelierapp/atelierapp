9371fcb734724aee49b580eb9d51c0b22045e184
Kenny Horna <kennyhorna@gmail.com>
2022-11-08 17:06:28 +0100

    Store re-enabled in PEru

--- resources/lang/es/cta.php
+++ resources/lang/es/cta.php
@@ -1,17 +1,9 @@
 <?php
 
 return [
-    'available-now' => 'Próximo lanzamiento en LATAM',
-    'get-it-on-app-store' => 'Únete a la lista de espera',
-    'get-it-for-ios' => 'Pronto en iOS',
+    'available-now' => 'Descargar ahora',
+    'get-it-on-app-store' => 'Descárgalo del App Store',
+    'get-it-for-ios' => 'Obtenla para iOS',
     'your-products-will-be-valued' => 'Con Atelier, tus productos siempre estarán frente a los <br> clientes que ya están buscando sus piezas únicas.',
     'open-your-shop' => 'Abrir mi tienda',
 ];
-
-//[
-//    'available-now' => 'Ya disponible!',
-//    'get-it-on-app-store' => 'Descárgala del App Store',
-//    'get-it-for-ios' => 'Obtenla para iOS',
-//    'your-products-will-be-valued' => 'Con Atelier, tus productos siempre estarán frente <br>a clientes que ya buscan piezas <br>únicas.',
-//    'open-your-shop' => 'Abrir mi tienda',
-//];
