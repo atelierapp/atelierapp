ddf5cadc69345c658fb9c6f79a1069f2f8402446
Kenny Horna <kennyhorna@gmail.com>
2022-11-10 00:28:25 +0100

    WIP

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -3,27 +3,19 @@
 namespace App\Providers;
 
 use Bouncer;
+use GeneaLabs\NovaTelescope\NovaTelescope;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
 
 class NovaServiceProvider extends NovaApplicationServiceProvider {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+
+    public function boot(): void
     {
         parent::boot();
     }
 
-    /**
-     * Register the Nova routes.
-     *
-     * @return void
-     */
-    protected function routes()
+    protected function routes(): void
     {
         Nova::routes()
             ->withAuthenticationRoutes()
@@ -31,13 +23,6 @@ class NovaServiceProvider extends NovaApplicationServiceProvider {
             ->register();
     }
 
-    /**
-     * Register the Nova gate.
-     *
-     * This gate determines who can access Nova in non-local environments.
-     *
-     * @return void
-     */
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
@@ -45,44 +30,26 @@ class NovaServiceProvider extends NovaApplicationServiceProvider {
         });
     }
 
-    /**
-     * Get the cards that should be displayed on the default Nova dashboard.
-     *
-     * @return array
-     */
-    protected function cards()
+    protected function cards(): array
     {
         return [
             // TODO: Create cards
         ];
     }
 
-    /**
-     * Get the extra dashboards that should be displayed on the Nova dashboard.
-     *
-     * @return array
-     */
-    protected function dashboards()
+    protected function dashboards(): array
     {
         return [];
     }
 
-    /**
-     * Get the tools that should be listed in the Nova sidebar.
-     *
-     * @return array
-     */
-    public function tools()
+    public function tools(): array
     {
-        return [];
+        return [
+            new NovaTelescope,
+        ];
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
         //
     }
