e53185d04b474da9c495808bf7c6101cc273fde9
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2022-11-22 21:19:14 -0500

    Updated process to accept payment order

---
+++ resources/lang/en/order.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'errors' => [
+        'accepted' => 'This order was accepted.',
+        'time_over' => 'The time to accept the order has ended.',
+    ],
+];
---
+++ resources/lang/es/order.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'errors' => [
+        'accepted' => 'Esta orden ya fue aceptada.',
+        'time_over' => 'El tiempo para aceptar la orden ha finalizado.',
+    ],
+];
