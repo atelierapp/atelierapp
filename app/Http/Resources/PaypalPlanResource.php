e77f88412d391fdd302815c655af84dca2d972b9
Kenny Horna <kennyhorna@gmail.com>
2022-11-29 19:06:24 -0500

    Fix: plan id included in response

--- app/Http/Resources/PaypalPlanResource.php
+++ app/Http/Resources/PaypalPlanResource.php
@@ -13,11 +13,12 @@ class PaypalPlanResource extends JsonResource
     public function toArray($request): array
     {
         return [
+            'id' => $this->id,
             'external_plan_id' => $this->external_plan_id,
             'name' => $this->name,
             'description' => $this->description,
             'frequency' => $this->frequency,
-            //'currency' => $this->currency,
+            'currency' => $this->currency,
             'price' => $this->price,
             'active' => $this->active,
         ];
