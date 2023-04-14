9bb251fe841e24fbac59ba0e3127db67112c83ff
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2023-04-13 22:31:05 -0500

    Fixed some tests

--- app/Models/Builders/ProductBuilder.php
+++ app/Models/Builders/ProductBuilder.php
@@ -41,7 +41,7 @@ class ProductBuilder extends Builder implements AuthUserContractBuilder
         }
 
         if (isset($filters['price-min']) && isset($filters['price-max'])) {
-            $this->filterByPriceRange($filters['price-min'], $filters['price-max']);
+            $this->filterByPriceRange($filters['price-min'] * 100, $filters['price-max'] * 100);
         }
 
         if (isset($filters['sort'])) {
--- tests/Feature/Dashboard/DashboardControllerTopProductsTest.php
+++ tests/Feature/Dashboard/DashboardControllerTopProductsTest.php
@@ -2,7 +2,12 @@
 
 namespace Tests\Feature\Dashboard;
 
+use App\Models\Order;
+use App\Models\OrderDetail;
 use App\Models\Product;
+use App\Models\Store;
+use Database\Seeders\OrderStatusSeeder;
+use Database\Seeders\PaymentStatusSeeder;
 
 class DashboardControllerTopProductsTest extends Basetest
 {
@@ -15,8 +20,23 @@ class DashboardControllerTopProductsTest extends Basetest
 
     public function test_an_seller_user_can_get_top_product()
     {
-        $this->createAuthenticatedSeller();
-        Product::factory()->count(10)->create();
+        $this->seed([
+            OrderStatusSeeder::class,
+            PaymentStatusSeeder::class,
+        ]);
+        $user = $this->createAuthenticatedSeller();
+        $store = Store::factory()->create(['user_id' => $user->id]);
+        Product::factory()->count(10)->create(['store_id' => $store->id])
+            ->each(function ($product) use ($user) {
+                $order = Order::factory()->create(['seller_id' => $user->id]);
+                OrderDetail::factory()
+                    ->count($this->faker->numberBetween(1,3))
+                    ->create([
+                        'order_id' => $order->id,
+                        'product_id' => $product->id
+                    ]);
+            });
+
         $response = $this->getJson(route('dashboard.top-product'), $this->customHeaders());
 
         $response->assertOk();
