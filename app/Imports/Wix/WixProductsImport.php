7da8778e899c97761017bfad3881433d5c81da36
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2023-11-01 19:28:25 -0500

    Improvement wix product import

--- app/Imports/Wix/WixProductsImport.php
+++ app/Imports/Wix/WixProductsImport.php
@@ -3,6 +3,8 @@
 namespace App\Imports\Wix;
 
 use App\Models\Product;
+use App\Models\Store;
+use App\Services\VariationService;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
@@ -14,20 +16,40 @@ class WixProductsImport implements ToCollection, WithHeadingRow
         $collection->each(function ($register) {
             $product = Product::withTrashed()->where('id', $register['sku'])->first();
             if (! is_null($product)) {
-                $properties = $product->properties;
-                $properties['wix'] = [
-                    'handleId' => $register['handleid'],
-                    'fieldtype' => $register['fieldtype'],
-                    'productImageUrl' => $register['productimageurl'] ?? '',
-                    'collection' => $register['collection'],
-                    'description' => $register['description'],
-                    'visible' => (string) $register['visible'] ? "true" : "false",
-                    'inventory' => $register['inventory'],
-                ];
-                $product->properties = $properties;
-                $product->title = $register['name'];
-                $product->save();
+                $this->setWixData($product, $register);
+            } else {
+                $store = Store::where('name', $register['brand'])->first();
+                if (! is_null($store)) {
+                    $product = Product::updateOrCreate([
+                        'store_id' => $store->id,
+                        'title' => $register['name'],
+                    ], [
+                        'active' => false,
+                        'sku' => $register['sku'],
+                        'price' => $register['price'],
+                        'description' => $register['description'],
+                    ]);
+                    $this->setWixData($product, $register);
+                    app(VariationService::class)->duplicateFromBaseProduct($product, []);
+                }
             }
         });
+    }
+
+    private function setWixData(Product $product, Collection $wixData): void
+    {
+        $properties = $product->properties;
+        $properties['wix'] = [
+            'handleId' => $wixData['handleid'],
+            'fieldtype' => $wixData['fieldtype'],
+            'productImageUrl' => $wixData['productimageurl'] ?? '',
+            'collection' => $wixData['collection'],
+            'description' => $wixData['description'],
+            'visible' => (string) $wixData['visible'] ? "true" : "false",
+            'inventory' => $wixData['inventory'],
+        ];
+        $product->properties = $properties;
+        $product->title = $wixData['name'];
+        $product->save();
     }
 }
